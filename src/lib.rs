c8b3b9b24d8278d36118ae20bfd43b402fef9f86
NXETCP <129570832+NXETCP@users.noreply.github.com>
2025-05-04 11:42:46 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,22 +18,16 @@ fn on_group_msg(e: Arc<MsgEvent>, bot: Arc<RuntimeBot>) {
         return;
     }
     
-    match text {
-        "/np menu" => bot.send_group_msg(e.group_id, MENU),
-        "/np images" => bot.send_group_msg(e.group_id, IMAGES),
-        "/np manage" => bot.send_group_msg(e.group_id, MANAGE),
-        _ => bot.send_group_msg(e.group_id, UNCMD)
+    if let Some(group_id) = e.group_id {
+        match text {
+            "/np menu" => bot.send_group_msg(group_id, MENU),
+            "/np images" => bot.send_group_msg(group_id, IMAGES),
+            "/np manage" => bot.send_group_msg(group_id, MANAGE),
+            _ => {}
+        }
     }
 }
 
-//菜单列表
 static MENU: &str = "MENU\n--\n -manage\n -images\n -others";
-
-//图片菜单
 static IMAGES: &str = "IMAGES\n--\n -cats\n -coser\n\n -search [key]";
-
-//群管菜单
 static MANAGE: &str = "MANAGE\n--\n -ban [uin] [time]\n -all_ban [off/on]";
-
-//无效指令
-static UNCMD: &str = "好像没有这个指令哦喵～";
